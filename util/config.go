f22a3e97fb8cc54233340db6aaf0feaa8ded7617
RYAN NGUYEN <nguyenthanhtai112005@gmail.com>
2026-04-21 11:35:16 +0700

    fix config.go

--- util/config.go
+++ util/config.go
@@ -21,6 +21,12 @@ func LoadConfig(path string) (config Config, err error) {
 
 	viper.AutomaticEnv()
 
+	viper.BindEnv("DB_DRIVER")
+	viper.BindEnv("DB_SOURCE")
+	viper.BindEnv("SERVER_ADDRESS")
+	viper.BindEnv("TOKEN_SYMMETRIC_KEY")
+	viper.BindEnv("ACCESS_TOKEN_DURATION")
+
 	err = viper.ReadInConfig()
 	if err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
