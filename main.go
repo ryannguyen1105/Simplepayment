766955f2455dc21cc7f3e54ab310de46ccb3b0df
RYAN NGUYEN <nguyenthanhtai112005@gmail.com>
2026-05-21 22:29:15 +0700

    feat: implement logic to start and run gRPC server

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	_ "github.com/lib/pq"
 	"github.com/ryannguyen1105/Simplepayment/api"
 	db "github.com/ryannguyen1105/Simplepayment/db/sqlc"
+	"github.com/ryannguyen1105/Simplepayment/gapi"
+	"github.com/ryannguyen1105/Simplepayment/pb"
 	"github.com/ryannguyen1105/Simplepayment/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:Fq9zkLWA2ZBAhq@localhost:5432/simple_payment?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:Fq9zkLWA2ZBAhq@localhost:5432/simple_payment?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
 	config, err := util.LoadConfig(".")
@@ -26,12 +31,37 @@ func main() {
 		log.Fatal("cannot connect to database:", err)
 	}
 	store := db.NewStore(conn)
+	// runGinServer(config, store)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimplePaymentServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start sever:", err)
 	}
