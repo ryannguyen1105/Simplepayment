fe84e0d17633fcc5ff9fe898a54a75ca7a00efaf
RYAN NGUYEN <nguyenthanhtai112005@gmail.com>
2026-06-08 17:35:29 +0700

    feat: implement gRPC parameter validation with localized error details

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -8,15 +8,21 @@ import (
 	db "github.com/ryannguyen1105/Simplepayment/db/sqlc"
 	"github.com/ryannguyen1105/Simplepayment/pb"
 	"github.com/ryannguyen1105/Simplepayment/util"
+	"github.com/ryannguyen1105/Simplepayment/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
-		if errors.Is(err,sql.ErrNoRows) {
+		if errors.Is(err, sql.ErrNoRows) {
 			return nil, status.Errorf(codes.NotFound, "user not found")
 		}
 		return nil, status.Errorf(codes.Internal, "failed to find user")
@@ -54,12 +60,22 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create session")
 	}
 	rsp := &pb.LoginUserResponse{
-		User: convertUser(user),
-		SessionID: session.ID.String(),
-		AccessToken: accessToken,
-		RefreshToken: refreshToken,
-		AccessTokenExpiresAt: timestamppb.New(accessPayload.ExpiredAt),
+		User:                  convertUser(user),
+		SessionID:             session.ID.String(),
+		AccessToken:           accessToken,
+		RefreshToken:          refreshToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiredAt),
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
 	}
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+	return violations
 }
